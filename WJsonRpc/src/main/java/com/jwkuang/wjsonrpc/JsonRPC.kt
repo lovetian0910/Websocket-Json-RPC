ab1c0dfe2d61f8478a1bbec5abb7a640b38cb947
jwkuang <jwkuang@tencent.com>
2021-09-29 17:10:55 +0800

    定义接口

--- WJsonRpc/src/main/java/com/jwkuang/wjsonrpc/JsonRPC.kt
+++ WJsonRpc/src/main/java/com/jwkuang/wjsonrpc/JsonRPC.kt
@@ -2,19 +2,33 @@ package com.jwkuang.wjsonrpc
 
 import org.java_websocket.client.WebSocketClient
 import org.java_websocket.handshake.ServerHandshake
+import org.json.JSONArray
+import org.json.JSONObject
 import java.lang.Exception
 import java.net.URI
+import java.util.concurrent.atomic.AtomicInteger
 
 /**
  * Created on 9/14/21
  * @author jwkuang
  */
 class JsonRPC(private val url: URI): WebSocketClient(url) {
-
+    private var requestId = AtomicInteger(1)
     fun start() {
         connect()
     }
 
+    suspend fun callServerMethod(method: String) {
+
+    }
+
+    suspend fun callServerMethod(method: String, params: JSONObject) {
+
+    }
+
+    suspend fun callServerMethod(method: String, params: JSONArray) {
+
+    }
 
     override fun onOpen(handshakedata: ServerHandshake?) {
         TODO("Not yet implemented")
